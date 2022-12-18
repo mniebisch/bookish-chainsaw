179853620ec6eacf60e2439518ebe09dd4b91547
mniebisch <michael.niebisch89@gmail.com>
2022-12-18 19:35:55 +0100

    Add quater center cases tests for orientation calculation where players start facing to the left.

--- tests/mmfl/feature/test_orientation.py
+++ tests/mmfl/feature/test_orientation.py
@@ -47,3 +47,15 @@ class TestCalcLeftStart:
             orientation=input_value
         )
         np.testing.assert_equal(expected_value, output_value)
+
+    @pytest.mark.parametrize(
+        "orientation_value, expected_value",
+        [(45, 135), (135, -135), (225, -45), (315, 45)],
+    )
+    def test_quater_center(self, orientation_value: int, expected_value: int) -> None:
+        input_value = pd.Series([orientation_value])
+        expected_value = np.array([expected_value])
+        output_value = orientation._calc_left_start(  # pylint: disable=protected-access
+            orientation=input_value
+        )
+        np.testing.assert_equal(expected_value, output_value)
