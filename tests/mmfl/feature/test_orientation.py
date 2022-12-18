8a2773159bea1868547af14cdb940d36ebe77fec
mniebisch <michael.niebisch89@gmail.com>
2022-12-18 18:29:29 +0100

    Add edge case tests for orientation calculation where players start facing to the right.

--- tests/mmfl/feature/test_orientation.py
+++ tests/mmfl/feature/test_orientation.py
@@ -1,6 +1,21 @@
+import numpy as np
+import pandas as pd
+import pytest
+
 from mmfl.feature import orientation
 
 
-class TestCalcLeftStart:
-    def test_blub(self):
-        assert True
+class TestCalcRightStart:
+    @pytest.mark.parametrize(
+        "orientation_value, expected_value",
+        [(0, -90), (90, 0), (270, -180), (360, -90)],
+    )
+    def test_edge_cases(self, orientation_value: int, expected_value: int) -> None:
+        input_value = pd.Series([orientation_value])
+        expected_value = np.array([expected_value])
+        output_value = (
+            orientation._calc_right_start(  # pylint: disable=protected-access
+                orientation=input_value
+            )
+        )
+        np.testing.assert_equal(expected_value, output_value)
